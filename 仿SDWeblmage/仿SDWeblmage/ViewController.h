d205f9f2b89740ea04a69e9fa5896d607611fb92
LY9264 <2859787456@qq.com>
2016-08-20 19:59:34 +0800

    创建项目

---
+++ 仿SDWeblmage/仿SDWeblmage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿SDWeblmage
+//
+//  Created by 9264 on 16/8/20.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 仿SDWeblmage/仿SDWeblmage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  仿SDWeblmage
+//
+//  Created by 9264 on 16/8/20.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
