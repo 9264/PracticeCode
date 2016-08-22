cc9ed39ba6a3ef3ef55a172c4a942eca516d55d0
LY9264 <2859787456@qq.com>
2016-08-22 19:33:09 +0800

    新建项目

---
+++ 异步下载网络图片/异步下载网络图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  异步下载网络图片
+//
+//  Created by 9264 on 16/8/22.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 异步下载网络图片/异步下载网络图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  异步下载网络图片
+//
+//  Created by 9264 on 16/8/22.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
