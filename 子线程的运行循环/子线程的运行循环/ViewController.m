048410f544d5f5ae33bed837a0f9b815a4e0c144
LY9264 <2859787456@qq.com>
2016-08-23 22:09:41 +0800

    完成

--- 子线程的运行循环/子线程的运行循环/ViewController.m
+++ 子线程的运行循环/子线程的运行循环/ViewController.m
@@ -19,6 +19,22 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self performSelector:@selector(timerDemo) withObject:nil];
+}
+
+- (void)timerDemo{
+    NSLog(@"LY");
+    NSTimer *time = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(demo) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:time forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop] run];
+    NSLog(@"JN");
+}
+
+- (void)demo{
+    NSLog(@"9264");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
