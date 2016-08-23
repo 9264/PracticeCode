82986b60a4944b4f2866d90821aba146569b8618
LY9264 <2859787456@qq.com>
2016-08-23 21:41:16 +0800

    完成

---
+++ 主线程的运行循环/主线程的运行循环/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  主线程的运行循环
+//
+//  Created by 9264 on 16/8/23.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 主线程的运行循环/主线程的运行循环/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  主线程的运行循环
+//
+//  Created by 9264 on 16/8/23.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self timrDemo];
+}
+
+- (void)timrDemo{
+    NSTimer *time = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(demo) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:time forMode:NSRunLoopCommonModes];
+}
+
+- (void)demo{
+    NSLog(@"JN");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
