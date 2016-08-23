60172c6538d09b724baa93df8ef0492cdb9a00fc
LY9264 <2859787456@qq.com>
2016-08-23 21:53:56 +0800

    新建项目

---
+++ 子线程的运行循环/子线程的运行循环/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  子线程的运行循环
+//
+//  Created by 9264 on 16/8/23.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
