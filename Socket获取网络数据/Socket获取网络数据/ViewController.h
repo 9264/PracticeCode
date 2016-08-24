d6b01ea060954991f51fd12801a4c7927ce05c0b
LY9264 <2859787456@qq.com>
2016-08-24 11:25:43 +0800

    创建项目

---
+++ Socket获取网络数据/Socket获取网络数据/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Socket获取网络数据
+//
+//  Created by 9264 on 16/8/24.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Socket获取网络数据/Socket获取网络数据/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Socket获取网络数据
+//
+//  Created by 9264 on 16/8/24.
+//  Copyright © 2016年 LY9264. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
