4eaa6a369a5ab9857946f8863bbf128edcb53e2d
LY9264 <2859787456@qq.com>
2016-08-29 10:59:49 +0800

    实现沙盒缓存

--- NSURLConnection下载文件/NSURLConnection下载文件/FileDownLoad.m
+++ NSURLConnection下载文件/NSURLConnection下载文件/FileDownLoad.m
@@ -9,14 +9,34 @@
 #import "FileDownLoad.h"
 @interface FileDownLoad ()<NSURLConnectionDataDelegate>
 @end
-@implementation FileDownLoad
+@implementation FileDownLoad{
+
+    NSMutableData *_dataM;
+
+}
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _dataM = [[NSData alloc]init];
+    }
+    return self;
+}
+
+
 - (void)downLoadWthURLString:(NSString *)URLString{
     
     NSURL *url = [NSURL URLWithString:URLString];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
     
+    [[[NSOperationQueue alloc] init] addOperationWithBlock:^{
+        
     [NSURLConnection connectionWithRequest:request delegate:self];
+        
+        [[NSRunLoop currentRunLoop] run];
+    }];
     
     
 //    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
@@ -38,11 +58,13 @@
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
     NSLog(@"%tu",data.length);
+    [_dataM appendData:data];
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
     NSLog(@"完成");
+    [_dataM writeToFile:@"/Users/LY/Desktop/jn1.zip" atomically:YES];
 }
 
 
